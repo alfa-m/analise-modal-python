a9ea804a3f65987604a6fc00bc2c2a9077e9b182
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-12-17 17:02:46 -0300

    atualiza código provisório

--- encontra_modo_critico.py
+++ encontra_modo_critico.py
@@ -2,20 +2,21 @@ import pandas as pd
 from procura_arquivos import procura
 
 matrizes_diagonais = procura("matriz_diagonal_Z_real*.csv", "./arquivos_csv_base/")
-modos_criticos = []
+lista_de_nos = pd.read_csv("./arquivos_csv_base/lista_de_nos.csv", index_col=0).drop([0,1,2])
+modos_criticos = pd.DataFrame(columns=["local","magnitude"])
 
 for matriz in matrizes_diagonais:
-    matriz_atual = pd.read_csv("./arquivos_csv_base/{}".format(matriz))
-    print("Matriz diagonal analisada:\n")
-    print(matriz_atual)
-    matriz_real_atual = matriz_atual.abs()
-    print("Matriz diagonal analisada:\n")
-    print(matriz_real_atual)
-    matriz_real_atual.describe()
-    print("Modo crítico:\n")
-    modo_critico = matriz_real_atual.to_numpy().max()
-    print(modo_critico)
-    modos_criticos.append(modo_critico)
+    matriz_atual = pd.read_csv("./arquivos_csv_base/{}".format(matriz), index_col=0)
+    matriz_atual.index = lista_de_nos
+    matriz_atual.columns = lista_de_nos
+    matriz_atual_complexa = matriz_atual.astype(complex)
+    matriz_atual_real = matriz_atual_complexa.abs()
+    valores_maximos = matriz_atual_real.max()
+    modo_critico = valores_maximos.max()
+    indice_modo_critico = [i for i, x in enumerate(valores_maximos) if x == modo_critico]
+    modos_criticos["local"] = lista_de_nos[indice_modo_critico]
+    modos_criticos["magnitude"] = modo_critico
 
-
-#df.max(numeric_only=True).max()
+print(modos_criticos)
+modos_criticos.index = matrizes_diagonais
+print(modos_criticos)
