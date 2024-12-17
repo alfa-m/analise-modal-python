c96ea150cb176cd5866a4f685c12dbcfc5d55438
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-12-17 18:37:42 -0300

    código de procura do modo crítico finalizado
    Lógica correta. Falta confirmar resultados

--- encontra_modo_critico.py
+++ encontra_modo_critico.py
@@ -3,9 +3,12 @@ from procura_arquivos import procura
 
 matrizes_diagonais = procura("matriz_diagonal_Z_real*.csv", "./arquivos_csv_base/")
 lista_de_nos = pd.read_csv("./arquivos_csv_base/lista_de_nos.csv", index_col=0).drop([0,1,2])
+lista_de_nos = lista_de_nos.reset_index(drop=True)
+lista_de_nos_numpy = lista_de_nos.to_numpy()
 modos_criticos = pd.DataFrame(columns=["local","magnitude"])
 
 for matriz in matrizes_diagonais:
+    indice_modo_critico = []
     matriz_atual = pd.read_csv("./arquivos_csv_base/{}".format(matriz), index_col=0)
     matriz_atual.index = lista_de_nos
     matriz_atual.columns = lista_de_nos
@@ -13,10 +16,8 @@ for matriz in matrizes_diagonais:
     matriz_atual_real = matriz_atual_complexa.abs()
     valores_maximos = matriz_atual_real.max()
     modo_critico = valores_maximos.max()
-    indice_modo_critico = [i for i, x in enumerate(valores_maximos) if x == modo_critico]
-    modos_criticos["local"] = lista_de_nos[indice_modo_critico]
-    modos_criticos["magnitude"] = modo_critico
+    indice_modo_critico = [i for i, x in enumerate(valores_maximos) if x == modo_critico][0]
+    no_modo_critico = lista_de_nos_numpy[indice_modo_critico]
+    modos_criticos.loc[matriz] = no_modo_critico, modo_critico
 
-print(modos_criticos)
-modos_criticos.index = matrizes_diagonais
-print(modos_criticos)
+modos_criticos.to_csv('./modos_criticos.csv')
